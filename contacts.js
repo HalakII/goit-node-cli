f67e57f66e0ef85a02586ac8360309dfb10022b6
HalakII <1017imiia@gmail.com>
2024-02-21 16:37:36 +0200

    finish

---
+++ contacts.js
@@ -0,0 +1,43 @@
+import { readFile, writeFile } from "fs/promises";
+import * as path from "path";
+import { nanoid } from "nanoid";
+import { json } from "stream/consumers";
+
+const contactsPath = path.join("./db", "contacts.json");
+console.log(contactsPath);
+
+export const listContacts = async () => {
+  const data = await readFile(contactsPath);
+  return JSON.parse(data);
+};
+
+export const getContactById = async (contactId) => {
+  const listOfContacts = await listContacts();
+  const needContact = listOfContacts.find(
+    (contact) => contact.id === contactId
+  );
+  return needContact || null;
+};
+
+export const addContact = async (name, email, phone) => {
+  const listOfContacts = await listContacts();
+  const addedContact = { id: nanoid(), name, email, phone };
+  listOfContacts.push(addedContact);
+  await writeFile(contactsPath, JSON.stringify(listOfContacts));
+  return addedContact;
+};
+
+export const removeContact = async (contactId) => {
+  const listOfContacts = await listContacts();
+  const deletedContact = listOfContacts.find(
+    (contact) => contact.id === contactId
+  );
+  if (deletedContact) {
+    const newContactList = listOfContacts.filter(
+      (contact) => contact.id !== contactId
+    );
+    await writeFile(contactsPath, JSON.stringify(newContactList));
+  }
+
+  return deletedContact || null;
+};
